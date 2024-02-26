ba9a7c23693f9508e722caaa06f8499b7c57e730
OMiquelini <miquelini.matheus@gmail.com>
2024-02-25 21:54:41 -0300

    Adding OrderStatus enum

---
+++ demo/src/main/java/com/miquelini/demo/Entities/Enums/OrderStatus.java
@@ -0,0 +1,33 @@
+package com.miquelini.demo.Entities.Enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(0),
+    PAID(1),
+    SHIPPED (2),
+    DELIVERED(3),
+    CANCELED (-1);
+
+    private int code;
+
+    private OrderStatus (int code)
+    {
+        this.code = code;
+    }
+
+    public int getCode()
+    {
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code)
+    {
+        for (OrderStatus value : OrderStatus.values())
+        {
+            if (value.getCode() == code)
+            {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code");
+    }
+}
--- demo/src/main/java/com/miquelini/demo/Entities/Order.java
+++ demo/src/main/java/com/miquelini/demo/Entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.miquelini.demo.Entities.Enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -32,14 +33,17 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private int orderStatus;
+
     public Order(){
     }
 
-    public Order (Long id, Instant moment, User client){
+    public Order (Long id, Instant moment, User client, OrderStatus orderStatus){
         super();
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId(){
@@ -59,6 +63,15 @@ public class Order implements Serializable {
     }
     public void setClient(User client){
         this.client = client;
+    }
+    public OrderStatus getOrderStatus(){
+        return  OrderStatus.valueOf(orderStatus);
+    }
+    public void setOrderStatus(OrderStatus orderStatus){
+        if(orderStatus != null)
+        {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public int hashCode(){
